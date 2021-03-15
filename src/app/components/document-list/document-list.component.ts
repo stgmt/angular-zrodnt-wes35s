45bcaece899d871fb3ba5844ac8a89dc64fda5a4
stgmt <stigmat.rudnev@gmail.com>
2021-03-15 20:27:48 +0300

    3 files modified

--- src/app/components/document-list/document-list.component.ts
+++ src/app/components/document-list/document-list.component.ts
@@ -1,33 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { DocumentService } from '../../services/document.service';
-import { Document } from '../../models/document';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { DocumentService } from "../../services/document.service";
+import { Document } from "../../models/document";
 @Component({
-  selector: 'app-document-list',
-  templateUrl: './document-list.component.html',
-  styleUrls: ['./document-list.component.css']
+  selector: "app-document-list",
+  templateUrl: "./document-list.component.html",
+  styleUrls: ["./document-list.component.css"]
 })
 export class DocumentListComponent implements OnInit {
   documents: Document[] = new Array<Document>();
+  sorted: boolean = false;
+  constructor(private docService: DocumentService) {}
 
-  constructor(
-      private docService: DocumentService,
-   
-    ) {}
+  sort() {
+    if (!this.sorted) {
+      this.sorted = true;
+      this.documents.sort(function(a, b) {
+        console.log(new Date(a.createdate).getTime());
 
+        return (
+          new Date(a.createdate).getTime() - new Date(b.createdate).getTime()
+        );
+      });
+      return;
+    }
 
-  loadDocuments(){
-    this.docService.getDocuments().subscribe(x=>{
+    this.sorted = false;
+
+    this.documents.sort(function(a, b) {
+      console.log(new Date(a.createdate).getTime());
+
+      return (
+        new Date(b.createdate).getTime() - new Date(a.createdate).getTime()
+      );
+    });
+  }
+
+  loadDocuments() {
+    this.docService.getDocuments().subscribe(x => {
       console.log(x);
       this.documents = x;
     });
   }
 
-  ngOnInit() {
-    
-    
-  }
-
+  ngOnInit() {}
 }
-
-
